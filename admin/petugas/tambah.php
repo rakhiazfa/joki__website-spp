1c789de850716a1b855f55574bc678775ba8c2cd
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-03-04 01:08:13 +0700

    Memperbaiki judul halaman.

--- admin/petugas/tambah.php
+++ admin/petugas/tambah.php
@@ -21,7 +21,7 @@ if ($_SESSION['user']['level'] !== 'admin') {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Edit Petugas | Pembayaran SPP</title>
+    <title>Tambah Petugas | Pembayaran SPP</title>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
