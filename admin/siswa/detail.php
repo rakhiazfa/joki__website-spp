7f121143bc73665227031e9e41523dff2d559d50
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-03-04 21:27:33 +0700

    Update detail siswa.

--- admin/siswa/detail.php
+++ admin/siswa/detail.php
@@ -13,14 +13,16 @@ if ($_SESSION['user']['level'] !== 'admin') {
     die();
 }
 
-$siswa = [];
-
-$query = mysqli_query($koneksi, "SELECT * FROM siswa LEFT JOIN kelas ON siswa.id_kelas = kelas.id_kelas ORDER BY nisn DESC");
-
-while ($row = $query->fetch_assoc()) {
-
-    array_push($siswa, $row);
+if (!isset($_GET['nisn']) || $_GET['nisn'] == '') {
+    header('Location:' . BASE_URL . '/admin/siswa');
+    die();
 }
+
+$nisn = $_GET['nisn'];
+
+$query = mysqli_query($koneksi, "SELECT * FROM siswa WHERE nisn = '$nisn' LIMIT 1");
+
+$siswa = $query->fetch_assoc();
 
 ?>
 
@@ -30,7 +32,7 @@ while ($row = $query->fetch_assoc()) {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Daftar Siswa | Pembayaran SPP</title>
+    <title>Detail siswa | Pembayaran SPP</title>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -65,7 +67,7 @@ while ($row = $query->fetch_assoc()) {
                     <div class="container-fluid">
                         <div class="row mb-2">
                             <div class="col-sm-6">
-                                <h1 class="m-0">Daftar Siswa</h1>
+                                <h1 class="m-0">Detail siswa</h1>
                             </div>
                         </div>
                     </div>
@@ -83,53 +85,12 @@ while ($row = $query->fetch_assoc()) {
 
                             <div class="card">
                                 <div class="card-header">
-                                    <h3 class="card-title">Daftar Siswa</h3>
-                                    <div class="card-tools">
-                                        <a href="<?php echo BASE_URL . '/admin/siswa/tambah.php' ?>" class="btn btn-primary">
-                                            Tambah Siswa
-                                        </a>
-                                    </div>
+                                    <h3 class="card-title">Detail siswa</h3>
                                 </div>
                                 <div class="card-body">
-                                    <table class="table table-bordered">
-                                        <thead>
-                                            <tr>
-                                                <th style="width: 10px">No</th>
-                                                <th>NISN</th>
-                                                <th>Nama Siswa</th>
-                                                <th>Kelas</th>
-                                                <th style="width: 40px">#</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <?php $nomor = 1 ?>
-                                            <?php foreach ($siswa as $item) { ?>
-                                                <tr>
-                                                    <td><?php echo $nomor++ ?></td>
-                                                    <td><?php echo $item['nisn'] ?? '' ?></td>
-                                                    <td><?php echo $item['nama'] ?? '' ?></td>
-                                                    <td>
-                                                        <?php echo ($item['nama_kelas'] ?? '') . ' ' . ($item['kompetensi_keahlian'] ?? '') ?>
-                                                    </td>
-                                                    <td>
-                                                        <div class="d-flex align-items-center" style="gap: 1rem;">
-                                                            <a class="btn btn-success" href="<?php echo BASE_URL . '/admin/siswa/detail.php?nisn=' . $item['nisn'] ?>">
-                                                                Detail
-                                                            </a>
 
-                                                            <a class="btn btn-primary" href="<?php echo BASE_URL . '/admin/siswa/edit.php?nisn=' . $item['nisn'] ?>">
-                                                                Edit
-                                                            </a>
+                                    <p>Nama : <?php echo $siswa['nama'] ?></p>
 
-                                                            <a class="btn btn-danger" onclick="return confirm('Yakin ingin menghapus data ini?')" href="<?php echo BASE_URL . '/admin/siswa/proses_hapus.php?nisn=' . $item['nisn'] ?>">
-                                                                Hapus
-                                                            </a>
-                                                        </div>
-                                                    </td>
-                                                </tr>
-                                            <?php } ?>
-                                        </tbody>
-                                    </table>
                                 </div>
                             </div>
 
